868eedc1a271841afc43f0d8752400fd517e7e89
Rimbaud Gregoire <rimbaudgregoire@hotmail.be>
2019-10-25 20:15:28 +0200

    Done with SignIn & SignUp drafts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
