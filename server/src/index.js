14216d1fcbf3a882d21037414ed76b82ceb9f635
amaury2409 <amaury.henrottay@gmail.com>
2019-10-25 19:59:07 +0200

    login

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 import {
-    login
+    login,
+    isLogin
 } from "./middleware/auth";
 
+import {
+    search
+} from "./middleware/question";
 
 const app = express();
 const port = 12345;
 
-
-
-
 app.use(bodyParser.json());
+
 app.post('/login', login);
-
-
-
+app.get('/search', search);
+app.use(isLogin);
 
 app.listen(port, () => {
     console.log("serveur 🚀🚀");
--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,14 +1,40 @@
+import {
+    sign
+} from "crypto";
+
+const jwt = require("jsonwebtoken");
+
 export const login = (req, res) => {
 
     console.log(req.body.username);
     if (req.body.username === "admin" && req.body.password === "admin") {
-        res.send("you are login");
+        // res.send("you are login");
+        const user = {
+
+            id: 0,
+            pseudo: "name",
+            id_user_type_fk: 2,
+
+        };
+        const token = jwt.sign(user, process.env.SECRET_KEY, {
+            expiresIn: '24h',
+        });
+        res.send(token);
 
     } else {
         res.send("Error in login");
     }
-    //id_user_type_fk
+}
 
+export const isLogin = (req, res, next) => {
 
+    try {
+        const decoded = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+        next();
+    } catch (err) {
+        res.send({
+            error: err.message
+        });
+    }
 
 }
