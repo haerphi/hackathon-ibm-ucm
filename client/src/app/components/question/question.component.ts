dd010bfc345e99fb32e8511b7b2c9f509cb8f1ae
Philippe Haerens <haerphi@gmail.com>
2019-10-26 16:16:48 +0200

    question dynamique

--- client/src/app/components/question/question.component.ts
+++ client/src/app/components/question/question.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import axios from "axios";
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  selector: "app-question",
+  templateUrl: "./question.component.html",
+  styleUrls: ["./question.component.scss"]
 })
 export class QuestionComponent implements OnInit {
+  id: number = -1;
+  info: any = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
+    this.getQuestion();
   }
 
+  async getQuestion() {
+    console.log("hello");
+    const rep = await axios.get(
+      `http://localhost/api/get_answer?id=${this.id}`
+    );
+    console.log(rep.data[0]);
+    this.info = rep.data[0];
+  }
+
+  ngOnInit() {}
 }
